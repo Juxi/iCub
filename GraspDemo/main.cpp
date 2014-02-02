cdf3001d0ee05d65d2080d638d3c62121e2f2600
Juxi <juxi@idsia.ch>
2014-02-02 17:11:07 +0100

    updating the reach for the grasp

--- GraspDemo/main.cpp
+++ GraspDemo/main.cpp
@@ -24,8 +24,18 @@ bool controlHand(bool open);
 bool openHand() { return controlHand(true); }
 bool closeHand(){ return controlHand(false); }
 
+bool addErrorForce(const char* objName);
+bool controlGraspObject(bool target, const char* objName);
+bool makeGraspTarget(const char* objName) {
+	return controlGraspObject(true, objName);
+}
+bool makeGraspObstacle(const char* objName) {
+	return controlGraspObject(false, objName);
+}
 
-std::string robot, part, objName;
+bool moveArm(double force);
+
+std::string robot, part;
 
 int main(int argc, char *argv[]) {
 	Network yarp;
@@ -39,7 +49,7 @@ int main(int argc, char *argv[]) {
 	part = "right_arm";	// whether or not to use the left hand (default is right)
 	if ( command.check("part") )  { part = command.find("part").asString().c_str(); }
 
-    objName = "cup1"; // the name of object in MoBeE
+    std::string objName = "cup1"; // the name of object in MoBeE
 	if ( command.check("object") )  { objName = command.find("object").asString().c_str(); }
 
 	std::string portName = "/" + robot + "/" + part + "/" + "graspdemo";	// the name of the RPC server
@@ -67,26 +77,29 @@ int main(int argc, char *argv[]) {
             case VOCAB_START:
                 response.addString("OK");
                 
-                // tell LeoGrasper to opn
+                // tell LeoGrasper to opn first and then close the hand
                 openHand();
-
-                // tell mobee to turn object into "tgt"
-//                help.addString("def");
-//                help.addString(objectName);
-//                help.addString("tgt");
-                
-                // tell LeoGrasper to cls
+                makeGraspTarget(objName.c_str());
+		addErrorForce(objName.c_str());
+                // wait two seconds
+                sleep(2);
                 closeHand();
+                sleep(2);
+                
+                moveArm(25.0);
+                                
                 break;
+                
             case VOCAB_STOP:
                 response.addString("OK");
-
-                // tell mobee to turn object into "obs"
-//                help.addString("def");
-//                help.addString(objectName);
-//                help.addString("obs");
                 
-                // tell LeoGrasper to opn
+                // tell LeoGrasper to opn and set the object as obstacle
+                openHand();
+                moveArm(0.0);
+				sleep(1);                
+                makeGraspObstacle(objName.c_str());
+                
+                
                 
                 break;
 
@@ -131,6 +144,52 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
+
+bool controlGraspObject(bool target, const char* objName) {
+	// tell mobee to turn object into "tgt"
+//                help.addString("def");
+//                help.addString(objectName);
+//                help.addString("tgt");
+	yarp::os::RpcClient mobeePort;
+
+    // the name of the RPC port used by the core module
+    std::string portName = "/MoBeE/world";
+    std::cout << "Trying to connect to MoBeEPort at: " << portName << std::endl;
+    
+
+    // trying to open our local port
+    if (! mobeePort.open("/GraspDemo/mobeeConnection")) {
+        std::cout << "GraspDemo" << ": Unable to open port " << ("/GraspDemo/mobeeConnection") << std::endl;
+        return false;
+    }
+    
+    // trying to connect to the LeoGrasper port
+    printf("Trying to connect to %s\n", portName.c_str());
+    if(! Network::connect("/GraspDemo/mobeeConnection", portName.c_str()) ) {
+        std::cout << "GraspDemo" << ": Unable to connect to port ";
+        std::cout << portName.c_str() << std::endl;
+        return false;
+    }
+    
+    yarp::os::Bottle cmd, response;
+	
+	cmd.addString("def");
+	cmd.addString(objName);
+    if(target)	cmd.addString("tgt");
+    else     	cmd.addString("obs");
+    
+	mobeePort.write(cmd, response);
+	if( response.get(0).isString() ) {
+        if(response.get(0).asString() == "OK") {
+            return true;
+        }else
+            return false;
+	}
+    
+    return false;
+}
+                
+
 bool controlHand(bool open) {
 	yarp::os::RpcClient leoGrasperPort;
 
@@ -168,4 +227,60 @@ bool controlHand(bool open) {
     
     
     return false;
+}
+
+
+bool moveArm(double force) {
+	yarp::os::BufferedPort<yarp::os::Bottle> mobeePort;
+
+    // the name of the RPC port used by the core module
+    std::string portName = "/MoBeE/" + part + "/" + "cmd:i";
+    std::cout << "Trying to connect to mobee port at: " << portName << std::endl;
+    
+
+    // trying to open our local port
+    if (! mobeePort.open("/GraspDemo/cmd:o")) {
+        std::cout << "GraspDemo" << ": Unable to open port " << ("/GraspDemo/cmd:o") << std::endl;
+        return false;
+    }
+    
+    // trying to connect to the LeoGrasper port
+    printf("Trying to connect to %s\n", portName.c_str());
+    if(! Network::connect("/GraspDemo/cmd:o", portName.c_str()) ) {
+        std::cout << "GraspDemo" << ": Unable to connect to port ";
+        std::cout << portName.c_str() << std::endl;
+        return false;
+    }
+    	
+    yarp::os::Bottle& cmd = mobeePort.prepare();
+    cmd.clear();
+    
+    cmd.addVocab(yarp::os::Vocab::encode("opsp"));
+    cmd.addString("left_hand");
+    
+    yarp::os::Bottle b;
+    b.addDouble(0.0);    b.addDouble(0.0);    b.addDouble(force);
+    
+    cmd.addList() = b;
+    mobeePort.write();
+    std::cout << "GraspDemo" << ": sent force!" << std::endl;
+    
+/*    sleep(5);
+
+
+	cmd = mobeePort.prepare();
+    cmd.clear();
+    cmd.addVocab(yarp::os::Vocab::encode("opsp"));
+    cmd.addString("left_hand");
+    
+    b.clear();
+    b.addDouble(0.0);    b.addDouble(0.0);    b.addDouble(0.0);  
+    cmd.addList() = b;
+    mobeePort.write();
+    std::cout << "GraspDemo" << ": no more force!" << std::endl;    
+*/
+	return true;
+}
+
+bool addErrorForce(const char* obj) {
 }
